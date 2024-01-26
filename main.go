3d59fa043566c172467e19ac257c3018c7d47009
Naren elumalai <narenelumalai@Narens-MacBook-Pro.local>
2024-01-26 14:52:55 +0530

    comments added

--- main.go
+++ main.go
@@ -62,14 +62,14 @@ func customerArrives() {
 	case waitingRoom <- struct{}{}:
 		fmt.Println("Customer arrived and took a seat.")
 		select {
-		case barberChair <- struct{}{}:
+		case barberChair <- struct{}{}: //will receive the barber signals
 			fmt.Println("Barber starts cutting hair.")
 			time.Sleep(time.Duration(CuttingTime) * time.Second)
 			fmt.Println("Barber finished cutting hair.")
-			<-barberChair
+			<-barberChair //will send barber chair signals
 		default:
 			fmt.Println("All barber chairs are occupied, customer waiting.")
-			<-waitingRoom
+			<-waitingRoom //will send waiting room signals
 		}
 	default:
 		fmt.Println("No available seats in the waiting room, customer leaves.")
@@ -82,12 +82,12 @@ func barber(id int) {
 		case <-closingChannel:
 			return
 		default:
-			fmt.Printf("Barber %d is waiting for a customer.\n", id)
+			fmt.Printf("Barber %d is waiting for a customer.\n", id) //waiting for the customer
 			select {
 			case <-waitingRoom:
-				fmt.Printf("Barber %d woke up and started cutting hair.\n", id)
+				fmt.Printf("Barber %d woke up and started cutting hair.\n", id) //customer will wakeup the barber and cuting the hair
 				time.Sleep(time.Duration(CuttingTime) * time.Second)
-				fmt.Printf("Barber %d finished cutting hair.\n", id)
+				fmt.Printf("Barber %d finished cutting hair.\n", id) //barber finished the cutting
 			default:
 				fmt.Printf("Barber %d is sleeping.\n", id)
 				time.Sleep(1 * time.Second)
